a8109f72d8ac74ad2a378a2456334fcbda54cb60
Uriel Martinez <martinezuriel651@gmail.com>
2023-11-07 12:00:37 -0600

    initialization of code

---
+++ Algorithms&BigOLab/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithms_BigOLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
